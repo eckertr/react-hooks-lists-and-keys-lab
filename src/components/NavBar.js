a5786f1c75a88ba496f10cc451ac4b69d8fff0f0
eckertr <rachel.eckert13@gmail.com>
2022-05-02 13:41:36 -0400

    commit tree fb7b36cb66cf3e529d8a2054fa6182fca2fa17f5

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,19 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+const listItems = links.map((link) => <li>{link}</li>)
+  return <nav>
+    <a href="#home">home</a>
+<a href="#about">about</a>
+<a href="#projects">projects</a>
+  </nav>;
 }
 
 export default NavBar;
+
+//In the `NavBar` component, there is an array with three strings representing
+//each link on the page. For each of those strings, create an `<a>` tag that looks
+//like this:
+
+//```jsx 
+//<a href="#home">home</a>
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,15 +1,21 @@
 import React from "react";
-
+import ProjectList from "./ProjectList";
+import user from "../data/user"
 function ProjectItem({ name, about, technologies }) {
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
+      <div className="technologies">{
+technologies.map(technology => <span key={technology}>{technology}</span>)}
+       </div>
     </div>
   );
 }
 
 export default ProjectItem;
+
+{/*In the `ProjectList` component, a prop of `projects` is being passed down from
+the `App` component with an array of objects. For each object in the array,
+render one `ProjectItem` component with the correct props. Use the `id` of the
+project for the `key` prop.*/}
